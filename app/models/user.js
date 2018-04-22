c4c3a998604648a8c4e030cc2f09d01d82002e2a
Thanos Sivridis <Jehutty@outlook.com>
2018-04-22 12:07:04 +0100

    starting the engines

---
+++ app/models/user.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+
+//user Schema
+var UserSchema = new Schema({
+    name: String,
+    username: {type: String, required: true, index:{unique:true}},
+    password: {type: String, required: true, select: false}
+});
+
+//hash the password before the user is saved
+
+UserSchema.pre('save', function (next) {
+    var user = this;
+
+    //hash the pw only if the pw has been changed or user is new
+    if(!user.isModified('password')) return next();
+
+    //generate the hash
+    bcrypt.hash(user.password, null, null, function(err, hash){
+        if(err) return next(err)
+
+        //change the pw to hashed version
+        user.password = hash;
+        next();
+    });
+});
+
+//method to compare a given password with the db hash
+UserSchema.methods.comparePassword = function(password){
+    var user = this;
+    return bcrypt.compareSync(password, user.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports ={
+    'port':process.env.port || 8000,
+    'database': 'mongodb://admin:mother1load1@ds249079.mlab.com:49079/db-nodeapi',
+    'secret':'extremelyWellKeptSecret'
+};
